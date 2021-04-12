d0ee8face1d017502a563bfba0c28e34d70433cf
dario.ruiz <dario.ruiz@alumnos.uneatlantico.es>
2021-04-12 19:31:31 +0200

    Invert numbers done

--- src/edaii/tema3/RecursiveFuncs.java
+++ src/edaii/tema3/RecursiveFuncs.java
@@ -6,7 +6,7 @@ public class RecursiveFuncs {
 		return invertDigits(Integer.toString(i), "");
 	}
 	public static int invertDigits(String i, String acc) {
-		return i.length() == 0 ? acc : invertDigits(i.subSequence(0, i.length()-2), acc.concat(i.charAt(i.length() - 1)));
+		return i.length() > 0 ? invertDigits((String) i.subSequence(0, i.length()-1), acc.concat(Character.toString(i.charAt(i.length() - 1)))) : acc.charAt(acc.length()-1) == '-' ? Integer.parseInt((String) acc.subSequence(0, acc.length()-1)) - 2 * Integer.parseInt((String) acc.subSequence(0, acc.length()-1)) : Integer.parseInt(acc);
 	}
 
 	public static int sumArrayElements(int[] is) {
