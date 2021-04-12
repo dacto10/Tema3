7d87dbe86212b81fc14597a8fe431db456dcedfc
dario.ruiz <dario.ruiz@alumnos.uneatlantico.es>
2021-04-12 19:46:19 +0200

    sum array y sum matrix done

--- src/edaii/tema3/RecursiveFuncs.java
+++ src/edaii/tema3/RecursiveFuncs.java
@@ -9,12 +9,19 @@ public class RecursiveFuncs {
 		return i.length() > 0 ? invertDigits((String) i.subSequence(0, i.length()-1), acc.concat(Character.toString(i.charAt(i.length() - 1)))) : acc.charAt(acc.length()-1) == '-' ? Integer.parseInt((String) acc.subSequence(0, acc.length()-1)) - 2 * Integer.parseInt((String) acc.subSequence(0, acc.length()-1)) : Integer.parseInt(acc);
 	}
 
-	public static int sumArrayElements(int[] is) {
-		return 0;
+	public static int sumArrayElements(int[] arr) {
+		return arr.length == 0 ? 0 : sumArrayElements(arr, 0, 0);
+	}
+	public static int sumArrayElements(int[] arr, int index, int acc) {
+		return index == arr.length-1 ? acc + arr[index]: sumArrayElements(arr, index + 1, acc + arr[index]);
 	}
 
-	public static int sumMatrixElements(int[][] is) {
-		return 0;
+	public static int sumMatrixElements(int[][] matrix) {
+		return matrix.length == 0 ? 0 : sumMatrixElements(matrix, 0, 0);
+	}
+	
+	public static int sumMatrixElements(int[][] matrix, int index, int acc) {
+		return index == matrix.length-1 ? acc + sumArrayElements(matrix[index], 0, 0) : sumMatrixElements(matrix, index + 1, sumArrayElements(matrix[index], 0, 0));
 	}
 
 	public static String request(TestRequestable requestable, int i) {
